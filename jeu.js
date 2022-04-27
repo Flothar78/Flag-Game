74b8307921fc2fc22ebf2a03ddc9db4ef13680ca
Flothar78 <79831565+Flothar78@users.noreply.github.com>
2022-04-27 18:00:05 +0200

    Commit OK

---
+++ jeu.js
@@ -0,0 +1,101 @@
+const colors = [
+  "blue",
+  "white",
+  "red",
+  "green",
+  "orange",
+  "black",
+  "yellow",
+
+  "cobalt",
+];
+
+const Btns = document.getElementsByClassName("btn");
+const stripeLeft = document.getElementById("stripeLeft");
+const stripeMiddle = document.getElementById("stripeMiddle");
+const stripeRight = document.getElementById("stripeRight");
+
+Btns[0].addEventListener("click", function onClick() {
+  function getRandomColors() {
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    console.log(randomColor);
+    let index = colors.indexOf(randomColor);
+    console.log(index);
+    colors.splice(index, 1);
+    return randomColor;
+  }
+  function giveColors() {
+    stripeLeft.className = getRandomColors() + "-stripe";
+    stripeMiddle.className = getRandomColors() + "-stripe";
+    stripeRight.className = getRandomColors() + "-stripe";
+  }
+  giveColors();
+  function refillArray() {
+    const left = stripeLeft.className.slice(0, -7);
+    const middle = stripeMiddle.className.slice(0, -7);
+    const right = stripeRight.className.slice(0, -7);
+    console.log(colors);
+    console.log(left, middle, right);
+    colors.push(left, middle, right);
+    console.log(colors);
+  }
+  refillArray();
+  function resultOfColors() {
+    if (
+      stripeLeft.className == "blue-stripe" &&
+      stripeMiddle.className == "white-stripe" &&
+      stripeRight.className == "red-stripe"
+    ) {
+      alert("Pour voir la FRANCE clique sur OK");
+    } else if (
+      stripeLeft.className == "green-stripe" &&
+      stripeMiddle.className == "white-stripe" &&
+      stripeRight.className == "red-stripe"
+    ) {
+      alert("Pour voir l'ITALIE clique sur OK");
+    } else if (
+      stripeLeft.className == "orange-stripe" &&
+      stripeMiddle.className == "white-stripe" &&
+      stripeRight.className == "green-stripe"
+    ) {
+      alert("Pour voir la CÔTE D'IVOIRE, clique sur OK");
+    } else if (
+      stripeLeft.className == "green-stripe" &&
+      stripeMiddle.className == "white-stripe" &&
+      stripeRight.className == "orange-stripe"
+    ) {
+      alert("Pour voir l'IRLANDE, clique sur OK");
+    } else if (
+      stripeLeft.className == "black-stripe" &&
+      stripeMiddle.className == "yellow-stripe" &&
+      stripeRight.className == "red-stripe"
+    ) {
+      alert("Pour voir la BELGIQUE, clique sur OK");
+    } else if (
+      stripeLeft.className == "red-stripe" &&
+      stripeMiddle.className == "yellow-stripe" &&
+      stripeRight.className == "green-stripe"
+    ) {
+      alert("Pour voir la GUINEE, clique sur OK");
+    } else if (
+      stripeLeft.className == "green-stripe" &&
+      stripeMiddle.className == "yellow-stripe" &&
+      stripeRight.className == "red-stripe"
+    ) {
+      alert("Pour voir le MALI, clique sur OK");
+    } else if (
+      stripeLeft.className == "red-stripe" &&
+      stripeMiddle.className == "white-stripe" &&
+      stripeRight.className == "red-stripe"
+    ) {
+      alert("Pour voir le PEROU, clique sur OK");
+    } else if (
+      stripeLeft.className == "cobalt-stripe" &&
+      stripeMiddle.className == "yellow-stripe" &&
+      stripeRight.className == "red-stripe"
+    ) {
+      alert("Pour voir le ROUMANIE, clique sur OK");
+    }
+  }
+  resultOfColors();
+});
